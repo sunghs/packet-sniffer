5db012c6b214647432af30905997868e0922767e
sunghs <sunghs9071@naver.com>
2020-07-13 19:19:40 +0900

    Initial commit

---
+++ src/main/java/sunghs/packet/sniff/PacketSnifferApplication.java
@@ -0,0 +1,13 @@
+package sunghs.packet.sniff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketSnifferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacketSnifferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sunghs/packet/sniff/PacketSnifferApplicationTests.java
@@ -0,0 +1,13 @@
+package sunghs.packet.sniff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacketSnifferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
