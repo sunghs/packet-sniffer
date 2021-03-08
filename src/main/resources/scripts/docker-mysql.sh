1ef66a424afb4c9291cda233cb53ad792cc84601
sunghs <sunghs9071@naver.com>
2021-03-09 08:20:08 +0900

    docker-mysql.sh 에 lower_case_table_names 설정 추가

--- src/main/resources/scripts/docker-mysql.sh
+++ src/main/resources/scripts/docker-mysql.sh
@@ -4,3 +4,8 @@
 # version : mysql 5.7
 
 docker run -d -p 10000:3306 -e MYSQL_ROOT_PASSWORD=password --name mysql mysql:5.7
+
+# lower_case_table_names 의 설정을 바꿔야 하는 경우
+
+/etc/mysql/conf.d/docker.cnf 를 열어 lower_case_table_names=1 를 추가한다.
+vim을 쓰기 위해서는 apt update && apt-get install vim 이 필요.
