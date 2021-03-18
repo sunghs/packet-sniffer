cf77a69aa583ff40c7f6211a1d1e5830966a9bf7
sunghs <sunghs9071@naver.com>
2021-03-18 09:25:08 +0900

    querydsl 테스트 추가

--- src/test/java/sunghs/packet/sniff/QuerydslRepositoryTest.java
+++ src/test/java/sunghs/packet/sniff/QuerydslRepositoryTest.java
@@ -1,19 +1,46 @@
 package sunghs.packet.sniff;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import sunghs.packet.sniff.annotation.AllConfigurationTests;
+import sunghs.packet.sniff.model.entity.PacketHistory;
+import sunghs.packet.sniff.repository.support.PacketHistoryRepositorySupport;
 
+@ActiveProfiles("local")
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @AllConfigurationTests
 @DataJpaTest
 @Slf4j
 public class QuerydslRepositoryTest {
 
-    @Test
-    public void findTest() {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private PacketHistoryRepositorySupport packetHistoryRepositorySupport;
+
+    @BeforeEach
+    public void beforeEach() {
+        final JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(entityManager);
+        this.packetHistoryRepositorySupport = new PacketHistoryRepositorySupport(jpaQueryFactory);
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {
+        200L, 300L, 400L, 500L, 600L
+    })
+    public void findTest(Long seq) {
+        PacketHistory packetHistory = packetHistoryRepositorySupport.findBySeq(seq);
+        Assertions.assertEquals(packetHistory.getSeq(), seq);
+        log.info("seq {} idx {} time {}", packetHistory.getSeq(), packetHistory.getIdx(), packetHistory.getSniffTime());
     }
 }
